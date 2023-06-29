e61c1107f6388bd9c91b37a4c78bbf33952535ed
Deepak-0007 <dy124507@gmail.com>
2023-06-29 23:40:43 +0530

    added changes

--- src/app/create-carrental/create-carrental.component.ts
+++ src/app/create-carrental/create-carrental.component.ts
@@ -1,32 +1,35 @@
-import { Component,OnInit} from '@angular/core';
-import { Carrental } from '../carrental';
+import { Component, OnInit } from '@angular/core';
+import {  Carrental} from '../carrental';
 import { CarrentalService } from '../carrental.service';
-import { Route, Router, RouterFeature } from '@angular/router';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-create-carrental',
   templateUrl: './create-carrental.component.html',
   styleUrls: ['./create-carrental.component.css']
 })
-export class CreateCarrentalComponent implements OnInit { 
-  carrental: Carrental =new Carrental();
-  constructor(private carrentalService : CarrentalService,private router: Router){ }
+export class CreateCarrentalComponent implements OnInit {
+  carrental:Carrental =new Carrental();
+  constructor(private carrentalService :CarrentalService,private router: Router){}
   ngOnInit(): void {
     
   }
   saveCarrental(){
     this.carrentalService.createCarrental(this.carrental).subscribe(data =>{
       console.log(data);
-      this.goToCarrentalList();
+      this.goTOCarrentalList();
     },
-    error => console.log(error))
-    }
-    goToCarrentalList(){
-      this.router.navigate(['/carrental'])
-    }
-    onSubmit(){
-      console.log(this.carrental);
-      this.saveCarrental();
-    }
+    error =>console.log(error));
   }
+  goTOCarrentalList(){
+    this.router.navigate(['/carrental'])
+
+  }
+  onSubmit(){
+    console.log(this.carrental);
+    this.saveCarrental();
+  }
+
+}
 
 
