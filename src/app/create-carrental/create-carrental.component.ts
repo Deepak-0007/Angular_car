193398e82d6d12672a6aa54fed28a2a179d8bd56
Deepak-0007 <dy124507@gmail.com>
2023-06-30 23:08:56 +0530

    added changes

--- src/app/create-carrental/create-carrental.component.ts
+++ src/app/create-carrental/create-carrental.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import {  Carrental} from '../carrental';
 import { CarrentalService } from '../carrental.service';
 import { Router } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-create-carrental',
   templateUrl: './create-carrental.component.html',
   styleUrls: ['./create-carrental.component.css']
 })
-export class CreateCarrentalComponent implements OnInit {
-  carrental:Carrental =new Carrental();
-  constructor(private carrentalService :CarrentalService,private router: Router){}
+export class CreateCarrentalComponent implements OnInit{
+  carrental: Carrental =new Carrental();
+  constructor(private carrentalService: CarrentalService,private router: Router){}
   ngOnInit(): void {
-    
+
   }
   saveCarrental(){
     this.carrentalService.createCarrental(this.carrental).subscribe(data =>{
