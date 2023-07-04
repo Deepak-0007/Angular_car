c80c03fceda5f3369bc243d3f641871177d6f584
Deepak-0007 <dy124507@gmail.com>
2023-07-04 22:45:14 +0530

    added changes

--- src/app/carrental-details/carrental-details.component.ts
+++ src/app/carrental-details/carrental-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Carrental } from '../carrental';
+import { ActivatedRoute } from '@angular/router';
+import { CarrentalService } from '../carrental.service';
 @Component({
   selector: 'app-carrental-details',
   templateUrl: './carrental-details.component.html',
   styleUrls: ['./carrental-details.component.css']
 })
-export class CarrentalDetailsComponent {
-
+export class CarrentalDetailsComponent implements OnInit {
+id!: number
+carrental!: Carrental
+constructor(private route: ActivatedRoute,private carrentalService: CarrentalService){ }
+ngOnInit(): void {
+  this.id-this.route.snapshot.params['id'];
+  this.carrental=new Carrental();
+  this.carrentalService.getEmployeeById(this.id).subscribe( data=>{
+    this.carrental=data;
+  });
+}
 }
